7a83d4adcf2778f5dfa5886b932e4ba1354af5d3
Andrey <noo_@bk.ru>
2026-03-23 00:27:29 +0300

    +55

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,15 @@
 import { io } from 'socket.io-client'
 
+console.log('Socket connecting to:', window.location.origin)
+
 const socket = io()
 
 socket.on('connect', () => {
   console.log('Socket connected')
+})
+
+socket.on('connect_error', (err) => {
+  console.log('Socket connect error:', err)
 })
 
 socket.on('disconnect', () => {
